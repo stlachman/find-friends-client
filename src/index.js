def0b7b3cd6b510f57d8ec862437b0e889f0fbc3
Seth Lachman <lachmanseth17@gmail.com>
2019-10-26 11:17:54 -0400

    Added layout component to app.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/styles";
+import Layout from "../src/components/Layout";
 import theme from "./theme";
 import App from "./App";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
-    <App />
+    <Layout>
+      <App />
+    </Layout>
   </ThemeProvider>,
   document.getElementById("root")
 );
